0688668c3805c6be87c2afb61573d1a18713d1d0
Gizmo675 <septier.romain@gmail.com>
2020-06-29 17:01:52 +0200

    create react app & materialize css navbar

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+// Components
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <Navbar />
+  )
+}
+
+export default App;
