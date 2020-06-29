da4114bb97ae76b7c16824628bf89946944fa374
Gizmo675 <septier.romain@gmail.com>
2020-06-29 17:11:29 +0200

    creating components & router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
 import './App.css';
 
 // Components
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path="/" >
+        <Home />
+      </Route>
+      <Route path="/Login">
+        <Login />
+      </Route>
+      <Route path="/Profile">
+        <Profile />
+      </Route>
+      <Route path="/Signup">
+        <Signup />
+      </Route>
+    </BrowserRouter>
   )
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = ()=>{
   return(
     <nav>
       <div className="nav-wrapper dark">
-        <a href="#" className="brand-logo left">Logo</a>
+        <a href="/" className="brand-logo left">InstaLike</a>
         <ul id="nav-mobile" className="right">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
+          <li><a href="/Login">Login</a></li>
+          <li><a href="/Signup">Signup</a></li>
+          <li><a href="Profile">Profile</a></li>
         </ul>
       </div>
     </nav>
