eeba08644c3038c0b78509b28fa96f886c0580c5
Gizmo675 <septier.romain@gmail.com>
2020-06-30 11:39:52 +0200

    creating login & signup page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
   return (
-    <h1>Login</h1>
+    <div className="mycard">
+      <div className="card auth-card">
+        <h2>InstaLike</h2>
+          <input type="text" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button className="btn waves-effect waves-light blue-grey" type="submit" name="action">Login</button>
+          <h5>
+            <Link to="/signup">Pas encore enregistré ?</Link>
+          </h5>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <h1>Signup</h1>
+    <div className="mycard">
+      <div className="card auth-card">
+        <h2>InstaLike</h2>
+          <input type="text" placeholder="Pseudo" />
+          <input type="text" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button className="btn waves-effect waves-light blue-grey" type="submit" name="action">Signup</button>
+          <h5>
+            <Link to="/login">Deja un compte ?</Link>
+          </h5>
+      </div>
+    </div>
   )
 }
 
